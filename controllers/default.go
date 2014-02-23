aa7aff3be660beb4de925e3709b34f8efe99a60e
Robert Xu <robxu9@gmail.com>
2014-02-22 21:27:05 -0500

    add endpoint instructions

--- controllers/default.go
+++ controllers/default.go
@@ -9,9 +9,22 @@ type MainController struct {
 	beego.Controller
 }
 
+type Description struct {
+	Type        string
+	Description string
+}
+
 func (this *MainController) Get() {
 
-	this.Data["json"] = bson.M{"Version": "0.0.1"}
+	this.Data["json"] = bson.M{
+		"Version": "0.0.1",
+		"Endpoints": bson.M{
+			"/":       Description{Type: "GET", Description: "Get version, endpoints, etc."},
+			"/create": Description{Type: "POST", Description: `Takes JSON for input (e.g. "{ "User" : "+100000000", "Name": "TestEvent" }") and returns the created event ID.`},
+			"/user":   Description{Type: "POST", Description: `Takes JSON for input (e.g. "{ "User": "+100000000" }") and returns user statistics.`},
+			"/:id":    Description{Type: "GET", Description: `Get a specific event. ':id' is the event ID.`},
+		},
+	}
 
 	this.ServeJson()
 }
